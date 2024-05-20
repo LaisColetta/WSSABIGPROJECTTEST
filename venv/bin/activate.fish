fa7f704d797e14d99a138b08a5d49c78f1afdab1
LaisColetta <laiscoletta@gmail.com>
2024-05-20 22:05:29 +0100

    commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/laiscoletta/Desktop/WSSA-Big-Project/venv"
+set -gx VIRTUAL_ENV "/Users/laiscoletta/Desktop/WSSABIGPROJECTTEST/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
