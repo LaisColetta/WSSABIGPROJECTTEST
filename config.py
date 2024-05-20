7e7f3864d530ce5706566572add0d365addd991e
LaisColetta <laiscoletta@gmail.com>
2024-05-20 23:50:43 +0100

    a

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class config:
     MYSQL_DATABASE_HOST = 'laispythonanywhere.mysql.pythonanywhere-services.com'
     MYSQL_DATABASE_USER = 'laispythonanywhe'
     MYSQL_DATABASE_PASSWORD = 'recipe123'
-    MYSQL_DATABASE_DB = 'lais$recipedb'
+    MYSQL_DATABASE_DB = 'laispythonanywhe$recipedb'
     API_KEY = '67442644f15763961b1408291b163751'
     API_ID = 'caa57a65'
